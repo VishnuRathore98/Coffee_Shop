aa760daed3feda51f85bf79b964d036948e3155b
VishnuRathore98 <rathorevishnu666@gmail.com>
2024-07-22 08:37:45 +0530

    Started working on ScanAndPayScreen. Note: Buggy carousel code

--- screens/new_member_flow/AccountScreen.js
+++ screens/new_member_flow/AccountScreen.js
@@ -1,93 +1,208 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import {Icon} from 'react-native-vector-icons/Ionicons';
+import { useState } from "react";
+import { StyleSheet, Text, View, Image, Switch, ScrollView, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useFonts, Poppins_600SemiBold, Poppins_400Regular, Poppins_500Medium } from "@expo-google-fonts/poppins";
 
-export default function AccountScreen(){
-    return (
-        <View style={styles.rootContainer}>
-{/* Account */}
-            <View style={{flexDirection:'row', justifyContent:'space-between', padding:20}}>
-                <View>
-                    <Text>Account</Text>
-                    <Text>Welcome John!</Text>
-                </View>
-                <View>
-                    <Image style={{height:40, width:40}} source={require('../../assets/usericon.png')}/>
-                </View>
-            </View>
+export default function AccountScreen() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
-        <View style={{padding:20}}>
-{/* Profile */}
+  return (
+    <View style={styles.rootContainer}>
+      <ScrollView>
+      {/* Account */}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems:'center',
+          padding: 20,
+          // borderBottomWidth:0.2,
+          // borderStyle:'dotted',
+        }}
+      >
+        <View>
+          <Text style={{fontSize:30, fontFamily:'Poppins_600SemiBold'}}>Account</Text>
+          <Text style={{fontSize:16, fontFamily:'Poppins_400Regular'}}>Welcome John!</Text>
+        </View>
+        <View>
+          <Image
+            style={{ height: 62, width: 62 }}
+            source={require("../../assets/usericon.png")}
+          />
+        </View>
+      </View>
 
-            <View style={{padding:10}}>
-                <Text>Profile</Text>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                    <Text>Personal Info</Text>
-                    {/* <Icon />        */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Cards & Payments</Text>
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Transaction History</Text>
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Privacy & Data</Text>
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Account ID</Text>
-                    {/* <Icon /> */}
-                </View>
-            </View>
-           
-{/* Security */}
-           
-            <View style={{padding:10}}>
-                <Text>Security</Text>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>2-factor authentication</Text>
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Face ID</Text>
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                <Text>Passcode Lock</Text>
-                    {/* <Icon /> */}
-                </View>
-            </View>
+      <View style={{ padding: 10 }}>
+        {/* Profile */}
 
-{/* Notification Preferences */}
-
-            <View style={{padding:10}}>
-                <Text>Notification Preferences</Text>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                {/* <Text></Text> */}
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                {/* <Text></Text> */}
-                    {/* <Icon /> */}
-                </View>
-                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                {/* <Text></Text> */}
-                    {/* <Icon /> */}
-                </View>
-            </View>
+        <View style={{ padding: 10 }}>
+          <Text style={{fontSize:20, fontFamily:'Poppins_600SemiBold', padding:10, borderBottomWidth:0.2,
+          borderStyle:'dotted',}} >Profile</Text>
+          <TouchableOpacity
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10, marginBottom:2  }}
+          >
+            <Text style={{fontSize:20, fontFamily:'Poppins_400Regular', color:'#434343'}} >Personal Info</Text>
+            <Icon name="information-circle-outline" size={24} color='#434343' />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10, marginBottom:2  }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Cards & Payments</Text>
+            <Icon name="card-outline" size={24} color='#434343' />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10, marginBottom:2  }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Transaction History</Text>
+            <Icon name="reader-outline" size={24} color='#434343' />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10, marginBottom:2  }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Privacy & Data</Text>
+            <Icon name="hand-left-outline" size={24} color='#434343' />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10, marginBottom:2  }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Account ID</Text>
+            <Icon name="id-card-outline" size={24} color='#434343' />
+          </TouchableOpacity>
         </View>
 
+        {/* Security */}
+
+        <View style={{ padding: 10 }}>
+          <Text style={{fontSize:20, fontFamily:'Poppins_600SemiBold', padding: 10, borderBottomWidth:0.2,
+          borderStyle:'dotted', }} >Security</Text>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >2-factor authentication</Text>
+            
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Face ID</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Passcode Lock</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
         </View>
-    );
+
+        {/* Notification Preferences */}
+
+        <View style={{ padding: 10 }}>
+          <Text style={{fontSize:20, fontFamily:'Poppins_600SemiBold', padding:10, borderBottomWidth:0.2,
+          borderStyle:'dotted',}} >Notification Preferences</Text>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Email notifications</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Push notifications</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            {/* <Text></Text> */}
+            {/* <Icon /> */}
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Special Offers</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            {/* <Text></Text> */}
+            {/* <Icon /> */}
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Event Reminders</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            {/* <Text></Text> */}
+            {/* <Icon /> */}
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between", alignItems:'center', padding:10 }}
+          >
+            <Text style={{fontSize:18, fontFamily:'Poppins_400Regular', color:'#434343'}} >Security Alerts</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#34C759" }}
+              thumbColor={isEnabled ? "#FFFFFF" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+            {/* <Text></Text> */}
+            {/* <Icon /> */}
+          </View>
+        </View>
+      </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    rootContainer:{
-        flex:1,
-        justifyContent:'flex-start',
-        // alignItems:'center',
-        backgroundColor:'#F6F2ED'
-    },
+  rootContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    // alignItems:'center',
+    backgroundColor: "#F6F2ED",
+  },
 });
--- screens/new_member_flow/ScanAndPayScreen.js
+++ screens/new_member_flow/ScanAndPayScreen.js
@@ -1,9 +1,95 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {Poppins_400Regular} from "@expo-google-fonts/poppins";
+import { useState } from "react";
+import Icon  from "react-native-vector-icons/Ionicons";
+import Carousel from 'react-native-reanimated-carousel';
+
+
 
 export default function ScanAndPayScreen(){
+    const [scanAndPaySelected, setScanAndPaySelected] = useState(true);
+    const [rewardSelected, setRewardSelected] = useState(false);
+
+    const views1 = [
+        {
+           imgUrl: "/home/vpsr/Desktop/React-Native/RNCourse/Coffee-Shop/Coffee_Shop/assets/paycards/defaultCard.png",
+           title: "Airport Cabs",
+        },
+        {
+           imgUrl: "/home/vpsr/Desktop/React-Native/RNCourse/Coffee-Shop/Coffee_Shop/assets/paycards/masterCard.png",
+           title: "Gift Cards",
+        },
+        {
+           imgUrl: "/home/vpsr/Desktop/React-Native/RNCourse/Coffee-Shop/Coffee_Shop/assets/paycards/visaCard.png",
+           title: "Hourly Stays",
+        },
+    ];
+
+    function scanAndPayHandler(){
+        if (!scanAndPaySelected) {
+            setScanAndPaySelected(true);
+            setRewardSelected(false);
+        }
+    }
+    function rewardHandler(){
+        if (!rewardSelected) {
+            setRewardSelected(true);
+            setScanAndPaySelected(false);
+        }
+    }
+    function renderItem1 ({ item }) {
+        return (
+           <View style={styles.renderItem1_parentView}>
+              <Image source={{ uri: item.imgUrl }} style={styles.renderItem1_img} />
+              <View style={styles.renderItem1_view1}>
+                 <Text style={styles.renderItem1_text1}>OFFERS</Text>
+              </View>
+              <View style={styles.renderItem1_view2}>
+                 <Text style={styles.renderItem1_text2}>{item.title}</Text>
+                 <TouchableOpacity>
+                    <Text style={styles.renderItem1_text3}>EXPLORE OFFERS</Text>
+                 </TouchableOpacity>
+              </View>
+            </View>
+        );
+     };
+
     return (
         <View style={styles.rootContainer}>
-            <Text>ScanAndPayScreen</Text>
+            <View style={{ flexDirection:'row', marginTop:20}}>
+            <TouchableOpacity style={{padding:10, borderBottomWidth:2, borderColor: scanAndPaySelected?'#024B1F':'#EFEBE9'}} onPress={scanAndPayHandler}>
+            <Text style={{fontSize:20, fontFamily:'Poppins_400Regular'}}>Scan & Pay</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{padding:10, borderBottomWidth:2, borderColor: rewardSelected?'#024B1F':'#EFEBE9'}} onPress={rewardHandler}>
+            <Text style={{fontSize:20, fontFamily:'Poppins_400Regular'}}>Rewards Only</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                {
+                    scanAndPaySelected
+                ?
+                    
+                        // Carousel will go in here
+                        <View style={{justifyContent:'center', alignItems:'center' }}>
+                        <Carousel
+                            loop
+                            width={300}
+                            height={500}
+                            // autoPlay={true}
+                            data={views1}
+                            scrollAnimationDuration={1000}
+                            onSnapToItem={(index) => console.log('current index:', index)}
+                            renderItem={renderItem1}
+                        />
+                    </View>
+                    
+                :
+                    <View style={{justifyContent:'center', alignItems:'center'}}>
+                        <Text style={{fontSize:50}}><Icon name="star-half-outline" size={50}/>1300</Text>
+                        <Icon name="qr-code-outline" size={200}/>
+                    </View>
+                }
+            </View>
         </View>
     );
 }
@@ -11,8 +97,50 @@ export default function ScanAndPayScreen(){
 const styles = StyleSheet.create({
     rootContainer:{
         flex:1,
-        justifyContent:'center',
+        // justifyContent:'center',
         alignItems:'center',
         backgroundColor:'#F6F2ED'
     },
+    renderItem1_parentView: {
+        backgroundColor: "#ffffff",
+        borderRadius: 18,
+        height: 250,
+        width: 350,
+        justifyContent: "space-around",
+        alignItems: "center",
+        overflow: "hidden",
+     },
+     renderItem1_view1: {
+        width: 80,
+        position: "absolute",
+        fontSize: 20,
+        top: 10,
+        right: 20,
+        backgroundColor: "#ffffff",
+        borderRadius: 18,
+        alignItems: "center",
+     },
+     renderItem1_view2: {
+        width: 350,
+        flexDirection: "row",
+        justifyContent: "space-around",
+     },
+     renderItem1_img: {
+        width: 350,
+        height: 200,
+     },
+     renderItem1_text1: {
+        fontWeight: "700",
+        color: "#000000",
+     },
+     renderItem1_text2: {
+        marginVertical: 10,
+        fontSize: 20,
+        fontWeight: "bold",
+     },
+     renderItem1_text3: {
+        marginVertical: 12,
+        color: "blue",
+        fontWeight: "bold",
+     },
 });
